36b2c5a136d98ddf0a58de8298ad26334d848acc
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-08 08:19:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,10 +13,13 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
- monopoly  =>{
-  :railroad  =>{
+ monopoly  => {
+  :railroad  => {
+    :rent_in_dollars
+    
+  },
   :pieces => 4
-  }
+
 }
 end
 
